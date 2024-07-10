7a6f14050040d91ed341018a7a586632ad31b900
Surendra Patel <surendra.patel@thinkhat.ai>
2024-07-10 15:30:23 +0530

    Initial Commit

---
+++ Login_Screen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Login_Screen
+//
+//  Created by surendra on 10/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Login_Screen/Login_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_ScreenApp.swift
+//  Login_Screen
+//
+//  Created by surendra on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Login_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
